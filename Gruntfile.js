6b3e253c34c8168dda69e06ab1d082173add01a8
Jill Molloy <hello@jillmolloy.com>
2014-11-05 21:04:55 -0800

    updates

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,21 +1,39 @@
 module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      options: {
+        mangle: false
+      },
+      my_target: {
+        files: {
+          'javascripts/application.min.js': ['javascripts/src/*.js']
+        }
+      }
+    },
     sass: {
       dist: {
+        options: {
+          style: 'compressed'
+        },
         files: {
           'stylesheets/application.css' : 'stylesheets/sass/application.scss'
         }
       }
     },
     watch: {
+      js: {
+        files: 'javascripts/src/*.js',
+        tasks: ['uglify']
+      },
       css: {
         files: '**/*.scss',
         tasks: ['sass']
       }
     }
   });
+  grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.registerTask('default',['watch', 'sass']);
+  grunt.registerTask('default',['watch', 'sass', 'uglify']);
 }
